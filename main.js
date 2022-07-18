9d2602928c45aca6738e42b72d89b15234dbf721
Laureano Oliva <65315757+laucha54321@users.noreply.github.com>
2022-07-17 23:34:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const ctx = canvas.getContext('2d');
 canvas.width = 600
 canvas.height = canvas.width
 
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
+
 class Cell{
     constructor({position,size}){
         this.position = position
@@ -19,11 +25,28 @@ class Cell{
     }
 }
 
+class Piece{
+    constructor({position,width}){
+        this.position = position
+        this.width = width
+        this.drawPiece()
+    }
+    drawPiece(){
+        ctx.beginPath()
+        ctx.arc(this.position.x+this.width/2,this.position.y+this.width/2,(this.width-10)/2,2*Math.PI,false)
+        ctx.lineWidth=2
+        ctx.fillStyle='Yellow'
+        ctx.fill()
+        ctx.stroke()
+    }
+
+}
 class Board{
     constructor({n}){
         this.size = n
         this.board = []
         this.createBoard()
+        this.piece = new Piece({position:{x:0,y:0},width:canvas.width/this.size})
     }
     createBoard(){
         for(let i=0;i<this.size;i++){
@@ -32,11 +55,72 @@ class Board{
                 this.board[i].push(new Cell({position:{x:i*(canvas.width/this.size),y:j*(canvas.height/this.size)},size:this.size}))
             }
         }
-        console.log(this.board)
-        console.log(this.board)
+        //console.log(this.board)
+    }
+    isSolved(){
+        for(let i=0;i<this.size;i++){
+            for(let j=0;j<this.size;j++){
+                if(this.board[i][j].condition == "Green"){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+    checkEmpty({position}){
+        if((position.x<this.size)&&(position.y<this.size)&&(position.x>=0)&&(position.y>=0)){
+            if((this.board[position.x][position.y].condition === "Green")){
+                return true
+            }else{
+                return false
+            }
+        }
+    }
+    setConditionRed({position}){
+        this.board[position.x][position.y].condition = "Red"
+        this.reDrawBoard()
+        this.piece.position = {x:position.x*this.piece.width,y:position.y*this.piece.width}
+        this.piece.drawPiece()
+    }
+    reDrawBoard(){
+        for(let i=0;i<this.size;i++){
+            for(let j=0;j<this.size;j++){
+                this.board[i][j].draw()
+            }
+        }
     }
 }
 
 
-
 const board = new Board({n:3})
+
+var i=0
+var j=0
+board.setConditionRed({position:{x:i,y:j}})
+var solutions = []
+
+
+async function solve(){
+    var arr = []
+    arr.push({x:i,y:j})
+    while(!board.isSolved()){
+        await delay(0.5)
+        if(board.checkEmpty({position:{x:i+1,y:j}})){
+            board.setConditionRed({position:{x:i+1,y:j}})
+            i = i+1
+        }else if(board.checkEmpty({position:{x:i-1,y:j}})){
+            board.setConditionRed({position:{x:i-1,y:j}})
+            i = i-1
+        }else if(board.checkEmpty({position:{x:i,y:j+1}})){
+            board.setConditionRed({position:{x:i,y:j+1}})
+            j = j+1
+        }else if(board.checkEmpty({position:{x:i,y:j-1}})){
+            board.setConditionRed({position:{x:i,y:j-1}})
+            j = j-1
+        }
+        arr.push({x:i,y:j})
+    }
+    console.log(arr)
+}
+solve()
+
