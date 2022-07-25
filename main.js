bfdf2d09662ceb0646d1e4927a4d6fedd5152546
Laureano Oliva <65315757+laucha54321@users.noreply.github.com>
2022-07-25 00:34:22 -0300

    Obtiene las posibilidades de tableros simples

--- main.js
+++ main.js
@@ -7,8 +7,11 @@ canvas.height = canvas.width
 class Board{
     constructor(n,position){
         this.size = n
+        this.moveHistory = []
+        this.solved = false
+        this.piecePosition
         this.board = this.createNewBoard()
-        this.piecePosition = this.setPiece(position)
+        this.setPiece(position)
     }
     createNewBoard(){
         var arr = []
@@ -22,7 +25,8 @@ class Board{
     }
     setPiece(position){
         this.board[position.x][position.y] = 'Red'
-        return position
+        this.moveHistory.push(position)
+        this.piecePosition = position
     }
     posibleMoves(){
         var arrPosibleMoves = []
@@ -43,24 +47,45 @@ class Board{
         }
         if(this.piecePosition.y-1>=0){
             if(this.board[this.piecePosition.x][this.piecePosition.y-1]==='Green'){
-                arrPosibleMoves.push({x:this.position.x,y:this.position.y-1})
+                arrPosibleMoves.push({x:this.piecePosition.x,y:this.piecePosition.y-1})
             }
         }
         return arrPosibleMoves
     }
     copyAsShallow(board_param){
-        this.size = board_param.size
+        this.size = [board_param.size]
+        this.moveHistory = [...board_param.moveHistory]
         for(let i=0;i<this.size;i++){
             for(let j=0;j<this.size;j++){
                 this.board[i][j]=board_param.board[i][j]
             }
         }
     }
-
+    isSolved(){
+        for(let i=0;i<this.size;i++){
+            for(let j=0;j<this.size;j++){
+                if(this.board[i][j]==='Green'){
+                    return false
+                }
+            }
+        }
+        return true
+    }
 }
-var board = new Board(2,{x:0,y:0})
+var boards = [new Board(2,{x:0,y:0})]
+solve(boards[0])
 
-var shallowcopy = new Board(2,{x:0,y:0})
-console.log(shallowcopy)
-console.log(board)
-console.log(board.posibleMoves())
+function solve(board_param){
+    var arr = []
+    var pos = board_param.posibleMoves()
+    for(let i=0; i<pos.length;i++){
+        var aux = new Board(2,{x:0,y:0})
+        aux.copyAsShallow(board_param) // hago un nuevo Board igual al anterior
+        aux.setPiece(pos[i])    // y muevo en ese board una de las posibilidades
+        arr.push(aux)          // y lo agrego al array
+    }   // genero un nuevo board por cada posibilidad es una ramificacion
+    console.log(arr)
+    for(let i=0; i<arr.length;i++){
+        solve(arr[i])
+    }
+}
